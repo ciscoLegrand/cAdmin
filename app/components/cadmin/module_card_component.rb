b5a477d52dd65754232902bed17a33a4316196d1
cisco <cisco.glez@gmail.com>
2021-09-08 15:56:13 +0200

    [feat] starting module card component

--- app/components/cadmin/module_card_component.rb
+++ app/components/cadmin/module_card_component.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
-class ModuleCardComponent < ViewComponent::Base
+class Cadmin::ModuleCardComponent < ViewComponent::Base
+  attr_reader :label, :field, :icon
+
+  def initialize( label:,field:,icon:)
+    @label = label 
+    @field = field 
+    @icon  = icon
+  end
+
+  def render?
+    @label.present? && @field.present? && @icon.present?
+  end
 
 end
