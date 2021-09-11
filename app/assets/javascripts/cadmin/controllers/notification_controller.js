9ba477699c10a879e1016da632dcebccc6d46aeb
ciscolegrand <cisco.glez@gmail.com>
2021-09-11 10:31:55 +0200

    [add] notification component with styles and stimulus controller

---
+++ app/assets/javascripts/cadmin/controllers/notification_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "stimulus"
+import Notification from 'https://cdn.skypack.dev/stimulus-notification'
+export default class extends Notification {
+    close(){
+        this.element.remove()
+    }
+}
---
+++ app/components/cadmin/notification_component.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class Cadmin::NotificationComponent < ViewComponent::Base
+  attr_reader :data, :type, :icon, :color_class
+
+  def initialize(notification:)
+    
+    @type = notification[0]
+    @data = prepare_data(notification[1])
+    @icon = icon
+    @color_class = color_class
+  end
+
+  private
+
+  def prepare_data(data)
+    case data
+    when Hash
+      data
+    else
+      { title: data }
+    end
+  end
+
+  def icon
+    case @type
+    # TODO: change notice by success styles
+    when 'notice'
+      inline_svg_tag('cadmin/icons/ico-check-circle.svg', class: 'icon is-medium is-success')
+    when 'error'
+      inline_svg_tag('cadmin/icons/ico-ban.svg', class: 'icon is-medium is-danger')
+    when 'alert'
+      inline_svg_tag('cadmin/icons/ico-exclamation-triangle.svg', class: 'icon is-medium is-warning')
+    else
+      inline_svg_tag('cadmin/icons/ico-information-circle.svg', class: 'icon is-medium is-warning')
+    end
+  end
+  
+  def color_class
+    case @type
+    when 'notice'
+      'success has-text-success'
+    when 'error'
+      'error has-text-danger'
+    when 'alert'
+      'alert has-text-warning'
+    else
+      'notice has-text-info'
+    end
+  end
+
+  def render?
+    data.present?
+  end
+
+end
