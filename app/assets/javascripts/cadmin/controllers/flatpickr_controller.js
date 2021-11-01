d0755e8b818f8e305de53f41ec55980ab8e67a92
ciscolegrand <cisco.glez@gmail.com>
2021-11-01 22:13:05 +0100

    [bug] flatpicker local spanish doesnt work

--- app/assets/javascripts/cadmin/controllers/flatpickr_controller.js
+++ app/assets/javascripts/cadmin/controllers/flatpickr_controller.js
@@ -1,11 +1,12 @@
 import flatpickr from 'https://esm.sh/stimulus-flatpickr?deps=stimulus@2.0.0';
 
-import { Spanish } from 'https://esm.sh/flatpickr/dist/l10n/es.js';
+// import { Spanish } from 'https://npmcdn.com/flatpickr@4.6.9/dist/l10n/es.js';
 export default class extends flatpickr {
   initialize() {
     // sets your language (you can also set some global setting for all time pickers)
     this.config = {
-      locale: Spanish,
+      //TODO: research for spanish locales
+      // locale: Spanish ,
       dateFormat: "d-m-Y",
       allowInput: true,
       position: "auto right"
