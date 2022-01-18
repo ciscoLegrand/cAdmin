0ee1d9854b2bfbab831deb1f6331ac4fd0ebb353
ciscoLegrand <cisco.glez@gmail.com>
2022-01-18 20:29:58 +0100

    [feat] stripe checkout payment

--- app/controllers/cadmin/checkout_controller.rb
+++ app/controllers/cadmin/checkout_controller.rb
@@ -1,37 +1,25 @@
 module Cadmin
   class CheckoutController < ApplicationController 
     def create
-      product = Service.friendly.find(params[:id])
+      product = Service.find(params[:id])
       category = product.main_service_id 
 
-      card = Stripe::PaymentMethod.create({
-        type: 'card',
-        card: {
-          number: '4242424242424242',
-          exp_month: 1,
-          exp_year: 2023,
-          cvc: '314',
-        },
-      })
-
-
       @session = Stripe::Checkout::Session.create({
+        payment_method_types: [
+          "card"
+        ],
         line_items: [{
           name: product.name,
           amount: (product.price*100).to_i,
           currency: "eur",
           quantity: 1
         }],
-        payment_method_types: [
-          "card"
-        ],
         mode: 'payment',
-        success_url: main_app.root_url  ,
-        cancel_url: main_app.root_url ,
+        success_url: root_url  ,
+        cancel_url: root_url ,
       })
-      respond_to do |format|
-        format.js
-      end
+      # STRIPE documentation to checkout payment process -> https://github.com/stripe-samples/checkout-one-time-payments/blob/master/server/ruby/server.rb#L70-L82
+      redirect_to @session.url
     end
   end
 end
