ab7e59c5ab37fdd6f918a4ca746d362b848793a3
ciscoLegrand <cisco.glez@gmail.com>
2022-01-26 22:51:37 +0100

    [refactor] initialize better cart session

--- app/controllers/cadmin/cart_items_controller.rb
+++ app/controllers/cadmin/cart_items_controller.rb
@@ -37,9 +37,9 @@ module Cadmin
       end
 
       def set_cart
-        @cart = Cart.find(session[:cart_id]) 
+        @cart ||= Cart.find(session[:cart_id]) 
         rescue ActiveRecord::RecordNotFound => e
-          @cart = Cart.create!(ip: request.remote_ip)
+          @cart ||= Cart.create!(ip: request.remote_ip)
           session[:cart_id] = @cart.id
       end
   end
