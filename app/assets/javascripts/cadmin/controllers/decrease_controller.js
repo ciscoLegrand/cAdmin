e13c245e7fe8ad94e4694c8da71e27f141ada283
ciscoLegrand <cisco.glez@gmail.com>
2022-01-26 22:50:50 +0100

    [style] change aside logo and behaviour to decrease size

--- app/assets/javascripts/cadmin/controllers/decrease_controller.js
+++ app/assets/javascripts/cadmin/controllers/decrease_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["hidetext", "main", "changeIcon", "sidebar","arrow"]
+  static targets = ["hidetext", "main", "changeIcon", "sidebar","arrow", "logoa", "logob"]
 	static values = {
 		right: String,
 		left: String
@@ -11,7 +11,11 @@ export default class extends Controller {
     
     if (this.sidebarTarget.classList.contains("sidebar__w16")) { 
       this.sidebarTarget.classList.remove("sidebar__w16")
-      this.sidebarTarget.classList.add("sidebar__w5")       
+      this.sidebarTarget.classList.add("sidebar__w5")
+      this.logoaTarget.classList.remove("is-size-4")
+      this.logoaTarget.classList.add("is-size-6")
+      this.logobTarget.classList.remove("is-size-3")
+      this.logobTarget.classList.add("is-size-6")
       this.mainTarget.classList.remove("main__is-10__is-offset-2")
       this.mainTarget.classList.add("main__w95")
       this.arrowTarget.setAttribute("src", this.rightValue);
@@ -21,6 +25,10 @@ export default class extends Controller {
       this.sidebarTarget.classList.remove("sidebar__w5")
       this.sidebarTarget.classList.add("sidebar__w16") 
       this.mainTarget.classList.remove("main__w95")
+      this.logoaTarget.classList.remove("is-size-6")
+      this.logoaTarget.classList.add("is-size-4")
+      this.logobTarget.classList.remove("is-size-6")
+      this.logobTarget.classList.add("is-size-3")
       this.mainTarget.classList.add("main__is-10__is-offset-2")
       this.arrowTarget.setAttribute("src",this.leftValue);
       this.hidetextTargets.forEach( (elem) => { elem.classList.remove("sidebar__w5__hidetext")  } )
