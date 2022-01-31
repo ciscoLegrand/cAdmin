77f5033e3726ac1555c1a4e400734ea1b361698f
ciscoLegrand <cisco.glez@gmail.com>
2022-01-31 22:27:47 +0100

    [refacto] delete after sign up and redirect to main_app aftersingin

--- app/controllers/cadmin/application_controller.rb
+++ app/controllers/cadmin/application_controller.rb
@@ -52,10 +52,6 @@ module Cadmin
       def set_breadcrumbs
         add_breadcrumb 'Inicio', root_path
       end
-
-      def after_sign_up_path_for(resource)
-        root_path
-      end
       
       def after_sign_in_path_for(resource)
         main_app.root_path
