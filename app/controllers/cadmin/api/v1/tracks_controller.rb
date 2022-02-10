1aefc0feb6a4fa03576e98e3805825f48cf0b18e
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-10 22:07:51 +0100

    [feat] search spotify playlists

--- app/controllers/cadmin/api/v1/tracks_controller.rb
+++ app/controllers/cadmin/api/v1/tracks_controller.rb
@@ -20,16 +20,24 @@ module Cadmin
         def random 
           s_tracks = RSpotify::Playlist.browse_featured.last.tracks 
           @tracks = s_tracks.map do |s_track|
-            Track.new_from_spotify_track(s_track)
-          end
+                      Track.new_from_spotify_track(s_track)
+                    end
           json_render @tracks
         end
 
         def search 
           s_tracks = RSpotify::Track.search(params[:q])
           @tracks = s_tracks.map do |s_track| 
-            Track.new_from_spotify_track(s_track)
-          end
+                      Track.new_from_spotify_track(s_track)
+                    end
+          json_render @tracks
+        end
+
+        def serch_by_playlist
+          s_tracks = RSpotify::Playlist.find(params[:q]).tracks 
+          @tracks = s_tracks.map do |s_track| 
+                      Track.new_from_spotify_track(s_track)
+                    end
           json_render @tracks
         end
       end
