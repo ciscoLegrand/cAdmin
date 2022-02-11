6ef1eaf30ec0dc83b9c4636ae604576ebd641006
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-11 22:34:20 +0100

    [rename] change migration file date creation to fixing creation of the schema

--- db/migrate/20211005131405_create_cadmin_event_types.rb
+++ db/migrate/20211005131405_create_cadmin_event_types.rb
@@ -1,7 +1,7 @@
 class CreateCadminEventTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :cadmin_event_types do |t|
-      t.string :name
+      t.string :name, null: false, unique: true
       t.float :salary, null: false, default: 0, precision: 10, scale: 2
       t.float :overtime_price, null: false, default: 0, precision: 10, scale: 2
       t.float :assemble, null: false, default: 0, precision: 10, scale: 2
