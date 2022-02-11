4b97481d6e7d449085e997b6a59c1ad07bdac8e5
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-11 22:32:53 +0100

    [refactor] migration event to reference users, carts, event types, and locations to better manage dependencies

--- db/migrate/20211005131408_create_cadmin_events.rb
+++ db/migrate/20211005131408_create_cadmin_events.rb
@@ -1,18 +1,18 @@
 class CreateCadminEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :cadmin_events do |t|
-      t.integer :customer_id, null: false, foreign_key: {to_table: :cadmin_users}
-      t.integer :employee_id, null: true, foreign_key: {to_table: :cadmin_users}
-      t.integer :cart_id, null: true, foreign_key: {to_table: :cadmin_carts}
-      t.string :status, null: false, default: 'pending'
-      t.string :title
-      t.string :type_name, null: false, default: 'boda'
+      t.references :customer, null: false, foreign_key: {to_table: :cadmin_users}
+      t.references :employee, null: true, foreign_key: {to_table: :cadmin_users}
       t.string :number, null: false, unique: true
-      t.date :date, null: false
-      t.integer :guests 
-      t.integer :place_id
       t.float :deposit, null: false, default: 0, precision: 10, scale: 2
       t.float :total_amount, null: false, default: 0, precision: 10, scale: 2
+      t.references :cart, null: true, foreign_key: {to_table: :cadmin_carts}
+      t.string :title
+      t.string :status, null: false, default: 'pending'
+      t.references :event_type, null: false, foreign_key: {to_table: :cadmin_event_types}
+      t.date :date, null: false
+      t.integer :guests 
+      t.references :place, null: true, foreign_key: {to_table: :cadmin_locations}
       t.boolean :charged, null: false, default: false
       t.boolean :employee_paid, null: false, default: false
       t.text :observations
