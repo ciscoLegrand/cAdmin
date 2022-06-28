66c35d5694848a1bdaa291dc067437b7ba6d078a
ciscoLegrand <tecnico@maigasoft.es>
2022-06-28 08:08:36 +0200

    [add] service translations, client and server validations

--- app/models/cadmin/service.rb
+++ app/models/cadmin/service.rb
@@ -9,6 +9,10 @@ module Cadmin
     
     belongs_to :main_service
     has_many :stock_by_dates
+
+    validates :name, :stock, :price, presence: true
+
+
     
     def no_vat 
       (self.price / "1.#{self.vat}".to_f).round(2)
