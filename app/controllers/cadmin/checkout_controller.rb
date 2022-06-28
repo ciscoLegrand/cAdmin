9d0f748ddd700baf8cfe737ef5d039afd704f512
ciscoLegrand <tecnico@maigasoft.es>
2022-06-28 22:36:12 +0200

    [refactor] redirects to main app on checkout success and cancel stripe payment, create customer stripe id

--- app/controllers/cadmin/checkout_controller.rb
+++ app/controllers/cadmin/checkout_controller.rb
@@ -2,6 +2,8 @@ module Cadmin
   class CheckoutController < ApplicationController 
     def create
       @product = Event.find(params[:id])
+      #! update role and create customer on stripe
+      current_cadmin_user.create_stripe_customer unless current_cadmin_user.stripe_customer_id.present?
       @session = Stripe::Checkout::Session.create({
         customer: current_cadmin_user.stripe_customer_id,
         payment_method_types: [
@@ -14,8 +16,8 @@ module Cadmin
           quantity: 1
         }],
         mode: 'payment',
-        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}"  ,
-        cancel_url: cancel_url ,
+        success_url: main_app.success_url + "?session_id={CHECKOUT_SESSION_ID}"  ,
+        cancel_url: main_app.cancel_url ,
       })
       # STRIPE documentation to checkout payment process -> https://github.com/stripe-samples/checkout-one-time-payments/blob/master/server/ruby/server.rb#L70-L82 
       redirect_to @session.url
@@ -23,8 +25,7 @@ module Cadmin
 
     def success 
       if params[:session_id].present?
-        #! update role and create customer on stripe
-        current_cadmin_user.create_stripe_customer if current_cadmin_user.user?
+        
         @cart.booked!
         @event = Event.find_by(cart_id:@cart.id)
         @event.book!
@@ -43,10 +44,10 @@ module Cadmin
           @cart = Cart.create!(ip: request.remote_ip)
           session[:cart_id] = @cart.id  
           puts exception.message
-          redirect_to cancel_url, error: "Payment was not successful -> #{exception.message}"
+          redirect_to main_app.cancel_url, error: "Payment was not successful -> #{exception.message}"
         end
       else
-        redirect_to cancel_url, alert: "Payment was not successful"
+        redirect_to main_app.cancel_url, alert: "Payment was not successful"
       end
     end
 
